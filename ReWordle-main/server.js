25796d0babebd15a727704d97b3a7092450bf59d
Yotam-D <d.yotam@gmail.com>
2022-03-25 15:28:24 +0300

    connect server to DB

--- ReWordle-main/server.js
+++ ReWordle-main/server.js
@@ -1,12 +1,34 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const knex = require('knex')
+const cors = require('cors')
 const app = express()
 dotenv.config()
 
 app.set('view engine', 'ejs')
+app.use(cors())
 app.use('/',express.static(__dirname + '/public'))
 
-app.listen (process.env.PORT, ()=>{
-    console.log(`listnening on port ${process.env.PORT}`);
+
+app.listen (process.env.PORT || 5000 , ()=>{
+    console.log(`listnening on port ${process.env.PORT} (Backup Port-5000)`);
 })
 
+console.log(process.env.DB_HOST);
+
+const db = knex({
+    client:'pg',
+    connection: {
+      host : process.env.DB_HOST,     
+      port: process.env.DB_PORT,              
+      user : process.env.DB_USER,       
+      password : process.env.DB_PASSWORD,          
+      database : process.env.DB_DATABASE
+        }
+    })
+
+db
+.select('word_text')
+.from('words')
+.then(data => console.log(data))
+.catch(err=>console.log('catch Err',err))
